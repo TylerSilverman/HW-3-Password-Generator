e7b4db9447dfbc3dbe375778e7214aafc5e175ea
Tyler Silverman <silverman.tyler@gmail.com>
2020-09-25 18:15:21 -0400

    edited the var randomPassword to generate a randomPassword

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword (){
+function generatePassword () {
 var generatePassword;
 
 var length = prompt ("How long is the password?");
 
-
-if (length < 8){
+if (length < 8) {
   alert("Password must be at least 8 characters.");
   return generatePassword ();
 }
 
-if (length > 128){
+if (length > 128) {
   alert("Password cannot be more then 128 characters.");
   return generatePassword ();
 }
@@ -20,18 +19,16 @@ if (length > 128){
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-
-var randomPassword = [letters];
+var randomPassword = [randomPassword];
 
 for (var i=0; i < length; i++) {
   var randomIndex = Math.floor(Math.random() * letters.length);
   console.log(randomIndex);
-  randomPassword.push(letters[randomIndex]);
+  randomPassword.push(letters [randomIndex]);
 }
 
-generatePassword = randomPassword
-return generatePassword;
-
+generatePassword = randomPassword;
+return generatePassword
 }
 
 // Write password to the #password input
