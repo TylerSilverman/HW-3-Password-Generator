5fdf45cbffa6f0e163135ce99eae954d460f9c29
Tyler Silverman <silverman.tyler@gmail.com>
2020-09-26 13:13:36 -0400

    added symbols and numbers

--- script.js
+++ script.js
@@ -17,9 +17,8 @@ if (length > 128) {
 }
 // Array -------
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var numbers = [];
-// var symbols - [];
-//____________________________________________________________
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "+"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 var randomPassword = [randomPassword];
@@ -27,7 +26,7 @@ var randomPassword = [randomPassword];
 for (var i=0; i < length; i++) {
   var randomIndex = Math.floor(Math.random() * letters.length);
   console.log(randomIndex);
-  randomPassword.push(letters [randomIndex]);
+  randomPassword.push(letters[randomIndex]);
 }
 
 // Functions-----
