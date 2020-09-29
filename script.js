e00e4ef3aa52b6c533abe105660768ef2b8b1162
Tyler Silverman <silverman.tyler@gmail.com>
2020-09-29 11:05:21 -0400

    added array for each category and added consoleInside functions for each array

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ if (length > 128) {
   alert("Password cannot be more then 128 characters and inclue Upper or Lower case letters, numbers or special characters.");
   return generatePassword ();
 }
-// Array -------
-var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "+", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Array to generate password-------
+var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "+"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 var randomPassword = [randomPassword];
@@ -27,10 +30,11 @@ for (var i=0; i < length; i++) {
   randomPassword.push(characters[randomIndex]);
 }
 
-// Functions-----
-// consoleInside(letters);
-// consoleInside();
-// consoleInside();
+// Functions to console Inside -----
+consoleInside(upperCase);
+consoleInside(lowerCase);
+consoleInside(numbers);
+consoleInside(characters);
 
 
 generatePassword = randomPassword;
