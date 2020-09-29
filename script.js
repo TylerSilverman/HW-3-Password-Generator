5f3e1da56a0e9b855bac88d5631e3f4c539ccc02
Tyler Silverman <silverman.tyler@gmail.com>
2020-09-29 15:05:02 -0400

    added formula to connect upper, lower, numbers, and symbols to generate the password

--- script.js
+++ script.js
@@ -27,19 +27,21 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Functions to console Inside -----
-// consoleInside(upperCase);
-// consoleInside(lowerCase);
-// consoleInside(numbers);
-// consoleInside(characters);
-
-
 var randomPassword = [randomPassword];
 
 for (var i=0; i < length; i++) {
+  var randomIndex = Math.floor(Math.random() * upperCase.length);
+  console.log(randomIndex);
+  randomPassword.push(upperCase[randomIndex]);
   var randomIndex = Math.floor(Math.random() * characters.length);
   console.log(randomIndex);
   randomPassword.push(characters[randomIndex]);
+  var randomIndex = Math.floor(Math.random() * lowerCase.length);
+  console.log(randomIndex);
+  randomPassword.push(lowerCase[randomIndex]);
+  var randomIndex = Math.floor(Math.random() * numbers.length);
+  console.log(randomIndex);
+  randomPassword.push(numbers[randomIndex]);
 }
 
 generatePassword = randomPassword;
